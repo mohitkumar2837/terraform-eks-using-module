8bca77257102bcef99b96e67c659ee3dcfb82b4b
Your Name <mohitkumr17082004@gmail.com>
2026-09-17 00:47:28 +0530

    w

--- eks.tf
+++ eks.tf
@@ -45,7 +45,7 @@ module "eks" {
       max_size     = 3
       desired_size = 2
 
-      instance_types = ["t2.medium"]
+      instance_types = ["t3.micro"]
       capacity_type  = "SPOT"
 
       tags = {
